6a52b8c906103a1950a7e7321150d3685f1be1fe
Maegan <maegan.parales@usjr.edu.ph>
2022-08-07 00:45:42 +0800

    created screens

---
+++ finalsparales/lib/screens/favorite_tasks_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../blocs/bloc_exports.dart';
+import '../models/task.dart';
+import '../test_data.dart';
+import '../widgets/tasks_list.dart';
+
+class FavoriteTasksScreen extends StatelessWidget {
+  const FavoriteTasksScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TasksBloc, TasksState>(
+      builder: (context, state) {
+        List<Task> tasksList = state.favoriteTasks;
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Chip(
+                  label: Text('${state.favoriteTasks.length} Tasks'),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TasksList(tasksList: tasksList),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
